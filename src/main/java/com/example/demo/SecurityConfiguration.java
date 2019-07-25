fcc4b0d6cd2acb99d7977ad4408ee252d18579d9
syhan11 <sueyoung.6311@gmail.com>
2019-07-25 16:34:10 -0400

    clean up

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -60,20 +60,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//                .withUser("dave")
-//                    .password(passwordEncoder().encode("begreat"))
-//                        .authorities("ADMIN")
-//                .and()
-//                .withUser("user")
-//                    .password(passwordEncoder().encode("password"))
-//                        .authorities("USER");
-//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsServiceBean()).passwordEncoder(passwordEncoder());
     }
+
 }
 
