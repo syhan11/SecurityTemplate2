e95adc2b9b2a0744c0be3e3b1afb7e94263dccab
syhan11 <sueyoung.6311@gmail.com>
2019-07-31 09:05:40 -0400

    security template

--- src/main/java/com/example/demo/SecurityTemplate2.java
+++ src/main/java/com/example/demo/SecurityTemplate2.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Samazon {
+public class SecurityTemplate2 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Samazon.class, args);
+		SpringApplication.run(SecurityTemplate2.class, args);
 	}
 
 }
